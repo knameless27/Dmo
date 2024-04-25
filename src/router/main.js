71a62d634b88050d92d2b6da3884ad68963045a7
knameless27 <knameless27@gmail.com>
2024-04-25 10:36:17 -0400

    fix: :bug: router web history

--- src/router/main.js
+++ src/router/main.js
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from "vue-router";
+import { createWebHistory, createRouter } from "vue-router";
 
 import HomePage from "@/views/main_page/HomePage.vue";
 import LoginForm from "@/views/auth/LoginForm.vue";
@@ -9,6 +9,6 @@ const routes = [
 ];
 
 export const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes,
 });
--- src/views/main_page/HomePage.vue
+++ src/views/main_page/HomePage.vue
@@ -1,3 +1,15 @@
 <template>
     <h1>joum</h1>
+    <button @click="goLogin()">waos</button>
 </template>
+
+<script>
+
+export default {
+    methods: {
+        goLogin() {
+            this.$router.push('/login')
+        }
+    }
+}
+</script>
